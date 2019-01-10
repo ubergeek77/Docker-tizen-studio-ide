0488f5592eb6bbd817327a51cfceb206594cfa64
ubergeek77 <ubergeek77@users.noreply.github.com>
2019-01-10 16:29:02 -0600

    Run `xhost +` when launch script is executed

--- desktop_scripts/tizen-run-in-container.sh
+++ desktop_scripts/tizen-run-in-container.sh
@@ -4,6 +4,8 @@ CONTAINER_NAME=tizen-studio-ide
 TIZEN_USER=tizen
 TIZEN_DIR=/home/${TIZEN_USER}/tizen-studio/
 
+xhost +
+
 [[ $(docker ps -f "name=${CONTAINER_NAME}" --format '{{.Names}}') == ${CONTAINER_NAME} ]] || (docker start ${CONTAINER_NAME} && sleep 5)
 
 docker exec -u ${TIZEN_USER} -d ${CONTAINER_NAME} bash -c "cd /home/${TIZEN_USER} && ${TIZEN_DIR}$1"
